dcb2cef5bc4546055c824403ed6c865de00a4cbd
Matthew Crowell <overthewelldesigns@gmail.com>
2019-12-19 00:51:31 -0700

    finished layout and all quattro content
    finished both mobile and larger device layout except for page switch, added all content to quattro page

---
+++ js/car-site.js
@@ -0,0 +1,144 @@
+function quattro1()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro1.jpg";
+return false;
+}
+function quattro2()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro2.jpg";
+return false;
+}
+function quattro3()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro3.jpg";
+return false;
+}
+function quattro4()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro4.jpg";
+return false;
+}
+function quattro5()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro5.jpg";
+return false;
+}
+function quattro6()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro6.png";
+return false;
+}
+function quattro7()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro7.jpg";
+return false;
+}
+function quattro8()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro8.jpg";
+return false;
+}
+function quattro9()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro9.jpg";
+return false;
+}
+function quattro10()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro10.jpg";
+return false;
+}
+function quattro11()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro11.jpg";
+return false;
+}
+function quattro12()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro12.jpg";
+return false;
+}
+function quattro13()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro13.jpg";
+return false;
+}
+function quattro14()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro14.jpg";
+return false;
+}
+function quattro15()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro15.jpg";
+return false;
+}
+function quattro16()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro16.jpg";
+return false;
+}
+function quattro17()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro17.jpg";
+return false;
+}
+function quattro18()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro18.jpg";
+return false;
+}
+function quattro19()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro19.jpg";
+return false;
+}
+function quattro20()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro20.jpg";
+return false;
+}
+function quattro21()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro21.jpg";
+return false;
+}
+function quattro22()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro22.jpg";
+return false;
+}
+function quattro23()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro23.jpg";
+return false;
+}
+function quattro24()
+{
+var img = document.getElementById("image");
+img.src="images/quattro/quattro24.jpg";
+return false;
+}
